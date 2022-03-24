7594d67161af141d932c55009d591510b14d9bf5
MEBIROUK Maher <mahermebirouk@gmail.com>
2022-03-24 03:59:44 +0100

    Initial commit

---
+++ Lab3ShelbyFamily/app/src/main/java/com/example/lab3shelbyfamily/NewActivityTest.kt
@@ -0,0 +1,11 @@
+package com.example.lab3shelbyfamily
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewActivityTest : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_test)
+    }
+}
